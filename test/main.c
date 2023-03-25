ee5031accd1d24ce8d70f0800dcca8dc2520965e
duncannMacleod <127741717+duncannMacleod@users.noreply.github.com>
2023-03-25 13:41:44 +0100

    actualisation 25/03

---
+++ test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+        printf("%c",219);
+    getchar();
+    return 0;
+}
---
+++ text/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+        printf(" ");
+        printf("bonjour");
+    return 0;
+}

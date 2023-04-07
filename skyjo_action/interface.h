2e6c5c05339f3972437dc4e0ec4e05ce8528be64
duncannMacleod <127741717+duncannMacleod@users.noreply.github.com>
2023-04-07 16:19:02 +0200

    ajout initialisation.h et .c

--- skyjo_action/interface.h
+++ skyjo_action/interface.h
@@ -5,5 +5,7 @@
 
 int menu_accueil()//à definir -> premier menu, permet de choisir le nombre de joueurs
 void plateau() //à definir affiche le plateau d'un joueur
+void Positionner_Curseur(int x, int y); //SSP technique
+void color (int couleurDuTexte, int couleurDuFond); //SSP technique
 
 #endif // INTERFACE_H_INCLUDED

ed98636f12f59716e2433c7a1303c8c9f6743571
duncannMacleod <127741717+duncannMacleod@users.noreply.github.com>
2023-04-07 16:08:24 +0200

    travail sur les declarations
    dans declaration.h

--- skyjo_action/declaration.h
+++ skyjo_action/declaration.h
@@ -1,6 +1,47 @@
 #ifndef DECLARATION_H_INCLUDED
 #define DECLARATION_H_INCLUDED
 
+#define CARTE_JOUEUR_NOMBRE_L 3             //dimension du plateau de jeu 3 lignes x 4 colonnes
+#define CARTE_JOUEUR_NOMBRE_C 4             //idem
 
+#define CARTE_PIOCHE_ACTION_NB 27           //il n'y a pas de limite sur le nombre de carte action dans le jeu d'un joueur, il peut donc avoir les 27 cartes dans son jeu
+#define CARTE_PIOCHE_ACTION_FACE_VISIBLE 4  //voir règle jeu
+
+#define CARTE_PIOCHE_NOMBRE_NB 120          //il y a en tout 120 cartes de jeu
+
+#define DIM_STR 20
+
+
+struct joueur
+{
+                char deck_nombre [CARTE_JOUEUR_NOMBRE_L][CARTE_JOUEUR_NOMBRE_C]; //prenant de -2 à 13 + pas de carte->30
+                char deck_nombre_cache [CARTE_JOUEUR_NOMBRE_L][CARTE_JOUEUR_NOMBRE_C]; //0 si carte caché, 1 si carte visible
+                char deck_action[CARTE_JOUEUR_ACTION_NB]; //prenant de 14 à 22
+                char prenom [DIM_STR]; //nom du joueur
+                char score; // 0 à 255
+
+
+};
+typedef struct joueur S_joueur
+
+
+struct pioche
+{
+                char nombre[CARTE_PIOCHE_NOMBRE_NB]; //tableau de la pioche des nombres à remplir et à "mélanger"
+                char nombre_defausse[CARTE_PIOCHE_NOMBRE_NB]; //prévoir de refaire la pioche nombre quand celle ci est épuisé.
+                char nombre_defausse_dessous[9]; //il ne peut avoir qu'au maximum 9 cartes sous la pioche par manche.
+                char nombre_nb; //dans l'absolu pas besoin, a discuter
+
+
+                char action[CARTE_PIOCHE_ACTION_NB]; //tableau de la pioche des actions à remplir et à "mélanger"
+                char action_visible[CARTE_PIOCHE_ACTION_FACE_VISIBLE]; //les 4 cartes acrtions présentées
+                char action_nb; //dans l'absolu pas besoin, à discuter
+
+
+
+                //après chaque manche, les pioches sont réinitialisées
+};
+
+typedef struct pioche S_pioche;
 
 #endif // DECLARATION_H_INCLUDED
---
+++ skyjo_action/interface.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+
+#include "declarations.h"
+
+int menu_accueil()//à definir -> premier menu, permet de choisir le nombre de joueurs
+void plateau() //à definir affiche le plateau d'un joueur
+
+#endif // INTERFACE_H_INCLUDED

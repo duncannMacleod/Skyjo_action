c2b2a7fb6890a40a1f48dcad40761a90ac0491a0
duncannMacleod <127741717+duncannMacleod@users.noreply.github.com>
2023-04-08 17:22:14 +0200

    ajout des fonctions initialisation

---
+++ skyjo_action/initialisation.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <conio.h>
+
--- skyjo_action/initialisation.h
+++ skyjo_action/initialisation.h
@@ -1,6 +1,10 @@
 #ifndef INITIALISATION_H_INCLUDED
 #define INITIALISATION_H_INCLUDED
 
+void creation_profil_joueur(int nb_j, S_joueur j);
+void initialisation_carte_nombre(S_pioche carte);
+void initialisation_carte_action(S_pioche carte);
+void initialisation_carte_pioche(S_pioche carte);
 
 
 #endif // INITIALISATION_H_INCLUDED
